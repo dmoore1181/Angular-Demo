9085f1035893eb3212a26f9db3b33c2e9bb4b000
David Moore <david.moore@yourcls.com>
2016-08-02 10:15:52 -0400

    Event List test

--- DemoApp/app/js/controllers/EventListController.js
+++ DemoApp/app/js/controllers/EventListController.js
@@ -1,6 +1,6 @@
 'use strict';
 
 eventsApp.controller('EventListController',
-function EventListController($scope, $location, eventData){
+function EventListController($scope, eventData){
     $scope.events = eventData.getAllEvents();
 })
