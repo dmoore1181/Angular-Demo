8cc9388f2438eed016eedccad6e2b649de4f8c90
David Moore <david.moore@yourcls.com>
2016-08-10 15:25:18 -0400

    dataService and logger service

--- Services in Depth/public/app/books/BooksController.js
+++ Services in Depth/public/app/books/BooksController.js
@@ -4,10 +4,13 @@
         .controller('BooksController', BooksController);
 
 
-    function BooksController() {
+    function BooksController(books, dataService, logger) {
 
-        var vm = this;
+        var vm = this; //short for view model
 
+        vm.appName = books.appName;
+        vm.allBooks = dataService.getAllBooks();
+        logger.output('BooksController has been created.');
     }
 
 
