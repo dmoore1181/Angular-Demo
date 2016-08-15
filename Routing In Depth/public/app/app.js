5cb29ff7209e2f6eb7a503cb509b24768d1bf1b3
David Moore <david.moore@yourcls.com>
2016-08-15 16:51:02 -0400

    basic routing

--- Routing In Depth/public/app/app.js
+++ Routing In Depth/public/app/app.js
@@ -10,9 +10,46 @@
             .when('/', {
                 controller: 'HomeController',
                 controllerAs: 'home',
-                template: '<h1>This is an inline template</h1>'
-            });
+                templateUrl: '/app/templates/home.html'
+            })
+            .when('/schools',{
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: '/app/templates/allSchools.html',
+                caseInsensitiveMatch: true
+            })
+            .when('/classrooms',{
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                 templateUrl: '/app/templates/allClassrooms.html'
+            })
+            .when('/activities',{
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: '/app/templates/allActivities.html'
+            })
+            .otherwise('/')
+        ;
 
+    }]);
+
+    app.run(['$rootScope', '$log', function($rootScope, $log){
+        $rootScope.$on('$routeChangeSuccess', function(event, current, previous){
+            $log.debug('successfully changed routes');
+
+            $log.debug(event);
+            $log.debug(current);
+            $log.debug(previous);
+        });
+
+        $rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
+            $log.debug('error changing routes');
+
+            $log.debug(event);
+            $log.debug(current);
+            $log.debug(previous);
+            $log.debug(rejection);
+        })
     }]);
 
 }());
