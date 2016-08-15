48059893436bbd5c905636237206756a6d27e28e
David Moore <david.moore@yourcls.com>
2016-08-15 16:11:06 -0400

    Adding project Routing In Depth

---
+++ Routing In Depth/public/app/app.js
@@ -0,0 +1,18 @@
+(function() {
+
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
+
+        $logProvider.debugEnabled(true);
+
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                template: '<h1>This is an inline template</h1>'
+            });
+
+    }]);
+
+}());
