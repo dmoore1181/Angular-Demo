97925d5e2faca6aa5ab51585ed8574f311d6f588
David Moore <david.moore@yourcls.com>
2016-08-19 10:10:04 -0400

    resharper changes

--- DirectiveFundamentals/DirectiveFundamentals/scripts/app/script.js
+++ DirectiveFundamentals/DirectiveFundamentals/scripts/app/script.js
@@ -21,7 +21,7 @@ angular.module('app').controller('mainCtrl', function($scope){
 
 angular.module('app').directive('userInfoCard', function() {
     return {
-        template: "Name: {{user.name}} <br /> <br><div ng-show='!!user.address'>Address: <br /> {{user.address.street}} <br /> {{user.address.city}} <br /> {{user.address.planet}}</div><br><div> Friends: <div ng-repeat='friend in user.friends'> {{friend}} </div></div>",
-        restrict: "E"
+        restrict: 'E',
+        templateUrl: 'userInfoCard.html'
     }
 })
