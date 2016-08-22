846cdf65caf33b111ad1416aa8bbefbb62a67b66
David Moore <david.moore@yourcls.com>
2016-08-22 14:07:10 -0400

    Adding x date adding decorator directive to pause video

---
+++ DirectiveFundamentals/DirectiveFundamentals/scripts/app/decoratorDirectivesScript.js
@@ -0,0 +1,50 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function($scope) {
+    $scope.messages = [];
+    $scope.handlePause = function (e) {
+        console.log(e);
+        $scope.messages.push({ text: 'paused!' + new XDate().toString() });
+        console.log('video was paused!');
+    };
+});
+
+angular.module('app')
+    .directive('spacebarSupport',
+        function() {
+            return{
+                restrict: 'A',
+                link: function(scope, element, attributes) {
+                    $('body')
+                        .on('keypress',
+                            function(evt) {
+                                var videoElement = element[0];
+                                if (evt.keyCode === 32) {
+                                    if (videoElement.paused) {
+                                        videoElement.play();
+                                    }
+                                    else {
+                                        videoElement.pause();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+angular.module('app')
+    .directive('eventPause',
+        function($parse) {
+            return{
+                restrict: 'A',
+                link: function (scope, element, attributes) {
+                    var fn = $parse(attributes['eventPause']);
+                    element.on('pause',
+                        function(event) {
+                            scope.$apply(function() {
+                                fn(scope, {evt: event});
+                            });
+                        });
+                }
+            }
+        });
