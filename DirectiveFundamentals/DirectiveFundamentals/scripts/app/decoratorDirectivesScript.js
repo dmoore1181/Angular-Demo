1c10d3a9fc2b45584a56b52e091942b7cb09cfdd
David Moore <david.moore@yourcls.com>
2016-08-22 15:05:52 -0400

    Buisness specific directive

--- DirectiveFundamentals/DirectiveFundamentals/scripts/app/decoratorDirectivesScript.js
+++ DirectiveFundamentals/DirectiveFundamentals/scripts/app/decoratorDirectivesScript.js
@@ -7,6 +7,53 @@ angular.module('app').controller('mainCtrl', function($scope) {
         $scope.messages.push({ text: 'paused!' + new XDate().toString() });
         console.log('video was paused!');
     };
+
+    $scope.data = { message: 'I have not been clicked' };
+    $scope.clickHandler = function(p) {
+        p.message = 'I have been clicked';
+    };
+
+    $scope.user1 = {
+        name: 'Luke',
+        selected: false
+    };
+});
+
+angular.module('app').directive('userClickSelect', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            element.on('click',
+                function() {
+                    scope.user.selected = !scope.user.selected;
+                    scope.$apply()
+                });
+        }
+    }
+});
+
+angular.module('app').directive('userTile', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            user: '='
+        },
+        templateUrl: 'userTile.html'
+    }
+});
+
+angular.module('app').directive('myClick', function($parse) {
+    return {
+        link: function(scope, el, attrs) {
+            var fn = $parse(attrs['myClick']);
+            el.on('click',
+                function() {
+                    scope.$apply(function() {
+                        fn(scope);
+                    });
+                });
+        }
+    }
 });
 
 angular.module('app')
