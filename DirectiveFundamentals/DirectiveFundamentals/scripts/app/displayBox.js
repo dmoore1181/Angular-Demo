441c8aa5572565a516ae4c580ee6ee37cc44fe74
David Moore <david.moore@yourcls.com>
2016-08-24 14:20:01 -0400

    Transclusion - question and answer

--- DirectiveFundamentals/DirectiveFundamentals/scripts/app/displayBox.js
+++ DirectiveFundamentals/DirectiveFundamentals/scripts/app/displayBox.js
@@ -2,6 +2,7 @@
 
 angular.module('app').controller('mainCtrl', function ($scope) {
     $scope.message = 'this is a message';
+    $scope.answers = { baseLocation: 'Yavin 4' };
 });
 
 angular.module('app').directive('displayBox', function () {
@@ -13,7 +14,20 @@ angular.module('app').directive('displayBox', function () {
             $scope.close = function() {
                 $scope.hidden = true;
             }
+           
         },
-        transclude: true
+        transclude: true,
+        scope: true
+    }
+});
+
+angular.module('app').directive('myQuestion', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'myQuestion.html',
+        transclude: true,
+        scope: {
+            questionText: '@q'
+}
     }
 });
