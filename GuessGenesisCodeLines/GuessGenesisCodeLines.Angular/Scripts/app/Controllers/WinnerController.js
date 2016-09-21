101b0b384364bc02a726151a348f822275b0f213
David Moore <david.moore@yourcls.com>
2016-09-20 20:29:14 -0400

    Added image

--- GuessGenesisCodeLines/GuessGenesisCodeLines.Angular/Scripts/app/Controllers/WinnerController.js
+++ GuessGenesisCodeLines/GuessGenesisCodeLines.Angular/Scripts/app/Controllers/WinnerController.js
@@ -5,24 +5,15 @@
 
     function answerListController(winnerResource, $resource) {
         var winner = this;
-
-        //winnerResource.$get(function (data) {
-        //    debugger;
-        //    winner.name = data.UserName;
-        //});
-
         var winnnerResourceType = $resource('http://localhost/GuessGenesisCodeLines.API/api/GetAnswer');
-
         var newWinner = new winnnerResourceType();
-
-      
         newWinner.$get()
-            .then(function (data) {
+            .then(function(data) {
                 debugger;
                 winner.name = data.UserName;
                 winner.guess = data.answer;
             })
-            .catch(function () {
+            .catch(function() {
                 debugger;
                 toastr.error('Save Failed. Only 1 entry per person.');
             });
